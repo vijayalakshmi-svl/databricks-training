5f37ffc473a333388aac243e7f6e9db4365ba335
vijayalakshmi-svl <vijayalakshmiseeram2@gmail.com>
2026-05-16 20:49:09 +0530

    Update schema.sql

--- week1/day4/schema.sql
+++ week1/day4/schema.sql
@@ -1,1 +1,24 @@
+-- Create Table
 
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    full_name VARCHAR(100),
+    email VARCHAR(100),
+    department VARCHAR(50),
+    city VARCHAR(50),
+    salary VARCHAR(20),
+    remarks VARCHAR(200)
+);
+
+-- Insert Sample Data
+
+INSERT INTO employees VALUES
+(1, 'Karthik Kondpak', 'karthik.k@gmail.com', 'Data Engineering', 'Hyderabad', '75000', ' Top performer '),
+
+(2, 'Veena Reddy', 'veena_r@company.com', 'Analytics', 'Bangalore', '65000', 'Excellent communication'),
+
+(3, 'Ravi kumar', 'ravi.kumar@org.in', 'Data Science', 'Chennai', '85000', 'Needs improvement'),
+
+(4, 'Anil', 'anil@abc.com', 'DEVOPS', 'Pune', '70000', NULL),
+
+(5, ' Suresh ', 'suresh@xyz.com', 'data engineering', ' hyderabad ', '60000', ' ');
